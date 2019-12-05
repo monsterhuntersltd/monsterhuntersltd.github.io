1b7149950cef8cbcc976270c3730c46790393faa
Danielle Scepanovic <dscepano@flemingcollege.ca>
2019-12-05 13:46:26 -0500

    adding content

--- JavaScriptMain.js
+++ JavaScriptMain.js
@@ -7,18 +7,6 @@ jQuery(document).ready(function(){
 });
 
 
-function QuoteUs(){
-	var form = document.getElementById('Monster Hunter Quote'); /* this code is from the textbook*/
-
-	addEvent(form, 'submit', function(e) {
-		e.preventDefault();
-		var elements = this.elements; 
-		var username = elements.username.value
-		var msg = 'Welcome' + username;
-		document.getElementById('main').textContent = msg;
-	});
-}; 
-
 /* Written by Danielle for Contact Us page, for the button to show up */
 function getQuote() { 		
 	var y = document.getElementById("quotepage");	/*getting information from .quotepage class*/
